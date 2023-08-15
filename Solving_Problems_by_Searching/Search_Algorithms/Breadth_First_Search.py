e245249ac6e87bc095fdbc3ccbfcae34c2236c18
Leandro Teixeira <leandroteixeira3@gmail.com>
2023-08-15 08:54:24 -0300

    [feat] Implemented DFS and the corresponding tests.

--- Solving_Problems_by_Searching/Search_Algorithms/Breadth_First_Search.py
+++ Solving_Problems_by_Searching/Search_Algorithms/Breadth_First_Search.py
@@ -1,2 +1,23 @@
-# TODO: Implement BFS Algorithm
-# TODO: Implement Queue Frontier
+from Common.treeNode import TreeNode
+
+
+def simple_breadth_first_search(tree: TreeNode, value):
+    explored = []
+    frontier = [tree]
+    while len(frontier) > 0:
+        current = frontier.pop(0)
+        if current.value == value:
+            aux = current
+            action_list = [aux.action]
+            while aux.parent is not None:
+                aux = aux.parent
+                action_list = [aux.action] + action_list
+            return action_list
+
+        explored.append(current.value)
+
+        for child in current.children:
+            if child.value not in explored and child not in frontier:
+                frontier.append(child)
+
+    return None
--- Solving_Problems_by_Searching/Search_Algorithms/Depth_First_Search.py
+++ Solving_Problems_by_Searching/Search_Algorithms/Depth_First_Search.py
@@ -1,13 +1,24 @@
 from Common.treeNode import TreeNode
 
 
-def simple_depth_first_search(tree: TreeNode, value):
-    if tree.value == value:
-        return [tree.action]
+def simple_depth_first_search(tree: TreeNode, value, explored=None):
+    explored = []
+    frontier = [tree]
 
-    for child in tree.children:
-        path = simple_depth_first_search(child, value)
-        if path is not None:
-            return [tree.action] + path
+    while len(frontier) > 0:
+        current = frontier.pop(-1)
+        if current.value == value:
+            aux = current
+            action_list = [aux.action]
+            while aux.parent is not None:
+                aux = aux.parent
+                action_list = [aux.action] + action_list
+            return action_list
+
+        explored.append(current.value)
+
+        for child in reversed(current.children):
+            if child.value not in explored and child not in frontier:
+                frontier.append(child)
 
     return None
